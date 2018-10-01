3bcfde09c4b02930b3b15d1a07abc19ba0f7d7d4
Anna Wall <joannawall@studiojaw.com>
2018-10-01 18:34:22 -0400

    fixed help

--- chatbot/commands/help.py
+++ chatbot/commands/help.py
@@ -10,8 +10,8 @@ def botHelp(body={}, roomId="", sender="", event={}):
         else:
             return "The command you are looking or can't be found, please try again."
     else:
-        return(bot.config.matrix["prefix"]+"help needs at least one argument - the name of another command, ie \""+bot.config.prefix+"help echo\"" \
-                "\nFor a list of available commands try \""+bot.config.matrix["prefix"]+"commands\".")
+        return bot.config.prefix+"help needs at least one argument - the name of another command, ie \""+bot.config.prefix+"help echo\"" \
+                "\nFor a list of available commands try \""+bot.config.prefix+"commands\"."
 
 bot.COMMANDLIST["help"] = botHelp
 bot.HELPLIST["help"] = "help - 1 accepted, returns information about the use of a given function ie \""+bot.config.prefix+"help help\"."
