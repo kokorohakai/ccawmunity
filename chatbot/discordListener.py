fb663c7d9942c78648d387c5d8d524bf7b704307
Anna Wall <joannawall@studiojaw.com>
2018-10-01 13:00:30 -0400

    I can't type today.

--- chatbot/discordListener.py
+++ chatbot/discordListener.py
@@ -21,8 +21,8 @@ async def on_message(message):
 
     # create responses for messages starting with prefix
     if len(output) > 0:
-        if len(output[0]) > 0 ):
-            if (output[0][0] == bot.config.prefix: 
+        if len(output[0]) > 0:
+            if (output[0][0] == bot.config.prefix:
                 command = output[0][1:]
 
                 # if the command is in our dictionary of functions, use it (from commands.py)
